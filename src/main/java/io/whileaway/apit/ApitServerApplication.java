1ad8ba4b8b753fe3c7198d83b040733a0e5c5f70
ThisSeanZhang <thisseanzhang@gmail.com>
2019-01-21 14:37:34 +0800

    init

---
+++ src/main/java/io/whileaway/apit/ApitServerApplication.java
@@ -0,0 +1,14 @@
+package io.whileaway.apit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApitServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApitServerApplication.class, args);
+	}
+
+}
+
