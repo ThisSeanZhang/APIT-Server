2ed23df8f4ae0302a426de594a4ecaf93777b818
ThisSeanZhang <thisseanzhang@gmail.com>
2019-03-08 16:34:33 +0800

    * 增加登出功能

--- src/main/java/io/whileaway/apit/account/controller/SessionController.java
+++ src/main/java/io/whileaway/apit/account/controller/SessionController.java
@@ -8,10 +8,7 @@ import io.whileaway.apit.base.ResultUtil;
 import io.whileaway.apit.base.enums.SessionKeyConstant;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
@@ -35,5 +32,12 @@ public class SessionController {
         Result<Developer> result = sessionService.createSession(createSession);
         session.setAttribute(SessionKeyConstant.CURRENT_DEVELOPER, result.getData());
         return result;
+    }
+
+    @DeleteMapping()
+    public Result deleteSession() {
+        session.removeAttribute(SessionKeyConstant.CURRENT_DEVELOPER);
+        session.invalidate();
+        return ResultUtil.success();
     }
 }
